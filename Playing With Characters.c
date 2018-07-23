69ccfb3b4a5ef5aa07afd377855769b508d5a16b
Rohit Goswami <lynus.mascot@gmail.com>
2018-07-23 22:18:14 +0530

    Create Playing With Characters.c

---
+++ Playing With Characters.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char c;
+    char a[30];
+    char str[20];
+    scanf("%c",&c);
+    scanf("%s",a); 
+    scanf("\n"); // their is \n in buffer therfor use this to avoid stop of string input for next variable
+    scanf("%[^\n]%*c", &str);
+    printf("%c\n%s\n%s",c,a,str); 
+    
+    
+    return 0;
+}
