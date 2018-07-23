fa0518015cd7955113de63229268b2554ec4d4d1
Rohit Goswami <lynus.mascot@gmail.com>
2018-07-23 23:28:59 +0530

    Create For Loop in C.c

---
+++ For Loop in C.c
@@ -0,0 +1,46 @@
+// Author: Rohit Goswami
+// Email: lynus.mascot@gmail.com
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    int i;
+    scanf("%d\n%d", &a, &b);
+
+    for(i=a;i<=b;i++){
+        
+            switch(i){
+                case 1: printf("one\n");
+                        break;
+                case 2: printf("two\n");
+                        break;
+                case 3: printf("three\n");
+                        break;
+                case 4: printf("four\n");
+                        break;
+                case 5: printf("five\n");
+                        break;
+                case 6: printf("six\n");
+                        break;
+                case 7: printf("seven\n");
+                        break;
+                case 8: printf("eight\n");
+                        break;
+                case 9: printf("nine\n");
+                        break;
+            }
+        if(i%2==0 && i>9) printf("even\n");
+        else if(i%2!=0 && i>9) printf("odd\n");
+    }
+        
+
+    return 0;
+}
+
