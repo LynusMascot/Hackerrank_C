cb30560a9415ec4944e52e63948b8234387240da
Rohit Goswami <lynus.mascot@gmail.com>
2018-07-23 23:07:39 +0530

    Update Students Marks Sum.c

--- Students Marks Sum.c
+++ Students Marks Sum.c
@@ -1,6 +1,26 @@
 // Author: Rohit Goswami
 // Email : lynus.mascot@gmail.com
 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  int i;
+  int sum=0;
+    if(gender=='b'){
+        for(i=0;i<=number_of_students;i+=2){
+           sum=sum+marks[i];
+        }
+    }
+    else if(gender=='g') {
+        for(i=1;i<=number_of_students;i+=2)
+          sum=sum+marks[i];
+    }
+   return sum;      
+}
+
 int main() {
     int number_of_students;
     char gender;
