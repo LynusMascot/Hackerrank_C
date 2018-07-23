cc89f2b3ca3fce21d1b41ed8036a533d58967352
Rohit Goswami <lynus.mascot@gmail.com>
2018-07-23 22:18:47 +0530

    Update Playing With Characters.c

--- Playing With Characters.c
+++ Playing With Characters.c
@@ -10,7 +10,7 @@ int main()
     char str[20];
     scanf("%c",&c);
     scanf("%s",a); 
-    scanf("\n"); // their is \n in buffer therfor use this to avoid stop of string input for next variable
+    scanf("\n"); // there is "\n" in buffer therfor use this to avoid stop of string input for next variable
     scanf("%[^\n]%*c", &str);
     printf("%c\n%s\n%s",c,a,str); 
     
